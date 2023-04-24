35d2f5a08f72fdb49506e4bf60b565da097c9e80
Dani Carnevale <dani.carnevale2013@gmail.com>
2023-04-24 02:18:24 -0300

    version 1.2
    -seccion galeria con responsive desing incluido
    -seccion contacto con validate js integrado, falta hacerle la accion para que muestre que el mail se envio
    -FALTA navbar responsive
    -FALTA formulario de procesos y muestra de datos del mismo

--- app.js
+++ app.js
@@ -19,4 +19,55 @@ function getNewImages() {
 
 getNewImages();
 
+
 newImageButton.addEventListener('click', getNewImages);
+
+/* VALIDATE JS FUNCIONES */
+
+const form = document.getElementById('form-contacto');
+
+function enviarFormulario(event) {
+  event.preventDefault();
+
+  const validacion = validate(form, {
+    nombre: {
+      presence: { message: 'El campo nombre es obligatorio.' },
+      length: { minimum: 3, message: 'El campo nombre debe tener al menos 3 caracteres.' }
+    },
+    email: {
+      presence: { message: 'El campo email es obligatorio.' },
+      email: { message: 'El campo email debe ser una dirección de correo electrónico válida.' }
+    },
+    mensaje: {
+      presence: { message: 'El campo mensaje es obligatorio.' },
+      length: { minimum: 10, message: 'El campo mensaje debe tener al menos 10 caracteres.' }
+    }
+  });
+
+  if (validacion === undefined) {
+    // formulario valido aca hay que hacer la accion con esos datos
+    console.log('Formulario válido, enviando...');
+  } else {
+    // formulario invalido 
+    for (const campo in validacion) {
+      const errores = validacion[campo];
+      const input = form.querySelector(`#${campo}`);
+      const mensajeError = form.querySelector(`#${campo}Help`);
+      input.classList.add('is-invalid');
+      mensajeError.innerHTML = errores.join('<br>');
+      mensajeError.classList.remove('d-none');
+    }
+  }
+}
+
+form.addEventListener('submit', enviarFormulario);
+
+// Remover mensaje de error al cambiar el contenido del campo
+form.querySelectorAll('.form-control').forEach(input => {
+  input.addEventListener('input', () => {
+    input.classList.remove('is-invalid');
+    const mensajeError = form.querySelector(`#${input.id}Help`);
+    mensajeError.innerHTML = '';
+    mensajeError.classList.add('d-none');
+  });
+});
