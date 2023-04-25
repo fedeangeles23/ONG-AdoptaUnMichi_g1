d7c3e6fc484c932f9de8e444d94a2ef1c9e23382
Dani Carnevale <dani.carnevale2013@gmail.com>
2023-04-25 11:25:51 -0300

    version 1.4.2
    Formulario de adopcion Meli integrado con validate.js y jquery para mostrar el resumen de los datos

--- formAdopcion.js
+++ formAdopcion.js
@@ -14,15 +14,73 @@ function enviarFormularioAdop(event) {
         presence: { message: ' es obligatorio.' },
         length: { minimum: 3, message: ' debe tener al menos 3 caracteres.' }
       },
-      dni: {
-        presence: { message: ' es obligatorio.' },
-        length: { is: 8, message: ' debe tener 8 digitos.' }
+      telefono:{
+        presence: {message: ' es obligatorio'},
+        length: {minimum: 6, message: ' debe tener al menos 6 digitos.'},
+        format: {pattern: /^\d+$/,message: ' debe contener solo caracteres numéricos'}
+      },
+      email:{
+        presence: {message: ' es obligatorio'},
+        email: {message: 'no es un mail valido'}
+      },
+      direccion: {
+        presence: {message: ' es obligatorio'},
+        length: {minimum: 5, message: ' debe tener al menos 5 caracteres'}
+      },
+      numero: {
+        presence: {message: ' es obligatorio'},
+        format: {pattern: /^\d+$/,message: ' debe contener solo caracteres numéricos'}
+      },
+      localidad: {
+        presence: {message: ' es obligatorio'},
+        length: {minimum: 5, message: ' debe tener al menos 5 caracteres'}
+      },
+      cp:{
+        presence: {message: ' es obligatorio'},
+        format: {pattern: /^\d+$/,message: ' debe contener solo caracteres numéricos'} 
       }
     });
   
     if (validacion === undefined) {
-      // formulario valido aca hay que hacer la accion con esos datos
-      console.log('Formulario válido, enviando...');
+      // formulario valido
+      $(document).ready(function() {
+        $('#formAdopcion').submit(function(event) {
+          event.preventDefault();
+          var nombre = $('#nombre').val();
+          var apellido = $('#apellido').val();
+          var mayor_edad = $('input[name=mayor_edad]:checked').val();
+          var vivienda = $('input[name=vivienda]:checked').val();
+          var patio = $('input[name=patio]:checked').val();
+          var rejas = $('input[name=rejas]:checked').val();
+          var telefono = $('#telefono').val();
+          var email = $('#email').val();
+          var direccion = $('#direccion').val();
+          var numero = $('#numero').val();
+          var localidad = $('#localidad').val();
+          var cp = $('#cp').val();
+          $('#formAdopcion').remove();
+          var resumen = 
+            'Nombre: ' + nombre + '<br>' +
+            'Apellido: ' + apellido + '<br>' +
+            '¿Tenés más de 18 años?: ' + mayor_edad + '<br>' +
+            '¿Vivís en una casa o departamento?: ' + vivienda + '<br>' +
+            '¿Tenés patio al aire libre?: ' + patio + '<br>' +
+            '¿Las ventanas cuentan con rejas de protección?: ' + rejas + '<br>' +
+            'Teléfono: ' + telefono + '<br>' +
+            'Email: ' + email + '<br>' +
+            'Dirección: ' + direccion + '<br>' +
+            'Número: ' + numero + '<br>' +
+            'Localidad: ' + localidad + '<br>' +
+            'Código Postal: ' + cp+ '<br><br>' +
+            'MUCHAS GRACIAS! NOS COMUNICAREMOS A LA BREVEDAD'
+            ;
+          var contenedorResumen = $('<div>').addClass('resumen');
+          var tituloResumen = $('<h2>').text('Los datos ingresados son:');
+          var parrafoResumen = $('<p>').html(resumen);
+          contenedorResumen.append(tituloResumen).append(parrafoResumen);
+          $('#adopta').append(contenedorResumen);
+        });
+      }); 
     } else {
       // formulario invalido 
       for (const campo in validacion) {
@@ -39,7 +97,7 @@ function enviarFormularioAdop(event) {
 formAdopcion.addEventListener('submit', enviarFormularioAdop);
 
 // Remover mensaje de error al cambiar el contenido del campo
-formAdopcion.querySelectorAll('.form-control').forEach(input => {
+formAdopcion.querySelectorAll('input').forEach(input => {
   input.addEventListener('input', () => {
     input.classList.remove('is-invalid');
     const mensajeError = formAdopcion.querySelector(`#${input.id}Help`);
